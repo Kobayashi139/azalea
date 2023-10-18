6b91523ecf27db5d5d15b073457531065b0e9435
Kobayashi139 <kobayashi.shi.00@gmail.com>
2023-10-18 22:56:19 +0900

    first commit

--- database/migrations/2023_10_07_054543_create_map_table.php
+++ database/migrations/2023_10_07_054543_create_map_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('map', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            #map 
         });
     }
 
