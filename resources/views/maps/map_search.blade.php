e4bd8ea3a6ccb84263fc0592ade52614e0ed9e9a
Kobayashi139 <kobayashi.shi.00@gmail.com>
2023-10-19 21:26:24 +0900

    map表示難航

--- resources/views/maps/map_search.blade.php
+++ resources/views/maps/map_search.blade.php
@@ -11,14 +11,35 @@
     <body class="antialiased">
         <div id="map" style="height:500px; width:800px;"></div>
         
-        <form>
-            <input type="text" size="55" id="search" value="池袋駅付近のカフェ" />
-            <input type="button" size="55" value="検索" onClick="SearchGo()" />
+        <form action="/search" method="post">
+            
+            <input type="text" size="55" id="location" name="location" value="池袋駅付近のカフェ" />
+            <input type="button" size="55" value="検索"  />
             <div id="map_canvas" style="width: 100%; height: 90%;"></div>
         </form>
-        
-        <script src="{{ asset('/public/js/map_search.js') }}"></script>
+        @if(isset($restaurants) && count($restaurants) > 0)
+            <div id="restaurant-list">
+                <h2>Restaurants in the Area</h2>
+                <ul>
+                    @foreach ($restaurants as $restaurant)
+                        <li>{{ $restaurant->name }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @else
+            <p>No restaurants found in the area.</p>
+        @endif
+      
         <script type="text/javascript" script src="https://maps.googleapis.com/maps/api/js?language=ja&region=JP&key=AIzaSyDec7mudcnoMhynGZbFhZAefE85sce6_NQ&libraries=places" charset="utf-8"></script>
- 
+        <script>
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById('map'), {
+                center: { lat: 37.7749, lng: -122.4194 }, // マップの初期位置
+                zoom: 10 // ズームレベル
+            });
+        
+          
+        }
+        </script>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReviewController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,17 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-   // return view('/maps/map');
-    return view('/maps/map_search');
-    return view('welcome');
-});
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/', [ReviewController::class, 'index'])->name('review.index');
+    Route::post('/search', [ReviewController::class, 'search'])->name('review.search');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
